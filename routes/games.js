6f97cf46ae2e08429e945d0c506a378ac778d7a2
Hugo  Calderon Delgado <hugocalderon@Hugos-MacBook-Pro.local>
2025-06-22 05:58:51 -0700

    add public games

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllGames, getGame, createGame, showAddGame, updateGame, deleteGame, joinGame } = require('../controllers/games');
+const { getAllGames, getGame, createGame, showAddGame, updateGame, deleteGame, joinGame, getPublicGames } = require('../controllers/games');
 
 router.route('/').post(createGame).get(getAllGames);
+router.route('/all').get(getPublicGames);
 router.route('/new').get(showAddGame);
 router.route('/edit/:id').get(getGame);
 router.route('/update/:id').post(updateGame);
